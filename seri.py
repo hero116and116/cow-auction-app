52b2e1b4089f148f092a6daa1b14363b6b82b647
🐏 <takahashiissei@takahashiisseinoMacBook-Air.local>
2026-08-27 11:00:54 +0900

    文字の大きさ調整

--- seri.py
+++ seri.py
@@ -530,7 +530,7 @@ st.markdown(
     
     /* 👇 ボタンの中の「文字」を直接指定して強制的に大きくする */
     .st-key-btn_switch_price button p, .st-key-btn_switch_buyer button p {
-        font-size: 36px !important;  /* ここで文字の大きさを変えられます */
+        font-size: 32px !important;  /* ここで文字の大きさを変えられます */
         font-weight: 900 !important;
         margin: 0 !important;
     }
